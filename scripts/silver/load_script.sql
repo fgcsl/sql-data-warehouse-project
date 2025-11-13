f3a6aeb7b04b5ff78928f8216adad50a1f584d3d
ABHISHEK KHATRI <60095100+fgcsl@users.noreply.github.com>
2025-11-13 17:41:07 +0530

    Update load_script.sql

--- scripts/silver/load_script.sql
+++ scripts/silver/load_script.sql
@@ -22,3 +22,33 @@ END AS prd_line,
 CAST (prd_start_dt AS DATE) AS prd_start_dt,
 CAST(LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt_test
 FROM bronze.crm_prd_info;
+
+
+
+
+/* check */
+select * from bronze.crm_sales_details where 
+sls_price != sls_sales * sls_quantity or
+sls_sales is NULL or sls_quantity is NULL or sls_price is NULL 
+or sls_sales <= 0 or sls_quantity <= 0 or sls_price <= 0;
+
+/* execution check */
+
+SELECT 
+sls_ord_num, 
+sls_prd_key, 
+sls_cust_id, 
+CASE WHEN sls_order_dt = 0 or LEN(sls_order_dt) !=8 THEN NULL
+ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+
+CASE WHEN sls_ship_dt = 0 or LEN(sls_ship_dt) !=8 THEN NULL
+ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END AS sls_ship_dt,
+CASE WHEN sls_due_dt = 0 or LEN(sls_due_dt) !=8 THEN NULL
+ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END AS sls_due_dt,
+sls_sales, 
+sls_quantity, 
+sls_price
+from bronze.crm_sales_details ;
